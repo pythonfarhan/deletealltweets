137583c9668ecad434c1442cee555b1add0a5459
farhan <farrrrrhan@gmail.com>
2018-08-28 22:10:18 +0700

    delete

--- _constants.py
+++ _constants.py
@@ -1,10 +1,10 @@
 
 # masukan consumer key dan token key akun twitter anda pada variabel di bawah ini
-consumer_key = '4UI47KPwD8uOuatYOtyvTGB98'
-consumer_secret = 'SwxEOVr35jEuRO0j5QJeFDArnH8UMmXUibC7s1KkSjBPLRiH4F'
-access_token = '1015631337431040000-JY1eWtwNRnLnUtn0CKt5UVUrk0vzKZ'
-access_token_secret = '6KElNkIZsS7memqhoQol0dXS32MyAJeiwOaCgyexIdtmI'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 # masukan hari, jam, menit pada variabel di bawah ini
-hari = 'selasa' # e.g: hari = 'senin'
+hari = '' # e.g: hari = 'senin'
 
